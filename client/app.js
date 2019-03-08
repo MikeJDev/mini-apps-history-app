bc36d9f0aeff644cb137d794084eb240cc6fff5d
MikeJDev <ukmjanes@gmail.com>
2019-03-08 17:25:07 -0600

    connected json-server to render the static file output from webpack. Also created a route for componentDidMount to display dates and descriptions from the /events route.

--- client/app.js
+++ client/app.js
@@ -3,9 +3,32 @@ import React from "react";
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      events: []
+    };
   }
+
+  componentDidMount() {
+    let url = "http://localhost:3000/events";
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        let events = data
+          .map((event, index) => {
+            return (
+              <div key={index}>
+                <p>Date: {event.date}</p>
+                <p>Description: {event.description}</p>
+              </div>
+            );
+          })
+          .slice(0, 1000);
+        this.setState({ events: events });
+      });
+  }
+
   render() {
-    return <h1>Whats going on, manhghghg?</h1>;
+    return <div className="events"> {this.state.events} </div>;
   }
 }
 export default App;
