dbd745374814c19f5ca9ef2275ef5f9e23e21560
MikeJDev <ukmjanes@gmail.com>
2019-03-09 11:34:20 -0600

    FEAT added a search feature

--- client/app.js
+++ client/app.js
@@ -1,46 +1,80 @@
 import React from "react";
 import axios from "Axios";
+import ReactPaginate from "react-paginate";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      events: []
+      events: [],
+      value: ""
     };
+    this.handleSearchChange = this.handleSearchChange.bind(this);
   }
-
-  // componentDidMount() {
-  //   let url = "http://localhost:3000/events?_page=1";
-  //   fetch(url)
-  //     .then(response => response.json())
-  //     .then(data => {
-  //       let events = data
-  //         .map((event, index) => {
-  //           return (
-  //             <div key={index}>
-  //               <p>Date: {event.date}</p>
-  //               <p>Event Description: {event.description}</p>
-  //             </div>
-  //           );
-  //         })
-  //         .slice(0, 1000);
-  //       this.setState({ events: events });
-  //     });
-  // }
 
   componentDidMount() {
     axios
       .get("http://localhost:3000/events?_page=1")
       .then(response => {
-        console.log("response:", response);
+        let data = response.data.map((event, index) => {
+          return (
+            <div key={index}>
+              <p> Date: {event.date}</p>
+              <p> Description: {event.description}</p>
+            </div>
+          );
+        });
+        this.setState({ events: data });
       })
       .catch(err => {
         console.log("err:", err);
       });
   }
 
+  search(e) {
+    let data = this.state.value;
+    event.preventDefault();
+    axios
+      .get(`http://localhost:3000/events?q=${data}`)
+      .then(response => {
+        let searchedData = response.data.map((event, index) => {
+          return (
+            <div key={index}>
+              <p>Date: {event.date}</p>
+              <p>Description: {event.description}</p>
+            </div>
+          );
+        });
+        this.setState({ events: searchedData });
+        console.log("searchedData:", searchedData);
+      })
+      .catch(err => {
+        console.log("err:", err);
+      });
+  }
+  handleSearchChange(e) {
+    this.setState({ value: e.target.value });
+  }
+
   render() {
-    return <div className="events"> </div>;
+    return (
+      <div>
+        <div>
+          <h1> Search for historical events</h1>
+          <form onSubmit={e => this.search(e)}>
+            <input
+              className="topnav"
+              type="text"
+              onChange={this.handleSearchChange}
+              value={this.state.value}
+              placeholder="Search..."
+            />
+            <input type="submit" />
+          </form>
+        </div>
+        <div className="events"> {this.state.events} </div>
+      </div>
+    );
   }
 }
 export default App;
