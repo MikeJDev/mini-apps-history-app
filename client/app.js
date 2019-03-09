08f2f333e323293a4f06371defd3f95ee1f35422
MikeJDev <ukmjanes@gmail.com>
2019-03-08 20:01:19 -0600

    created an axios route to retrieve the first 10 items in the events array

--- client/app.js
+++ client/app.js
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "Axios";
 
 class App extends React.Component {
   constructor(props) {
@@ -8,27 +9,38 @@ class App extends React.Component {
     };
   }
 
+  // componentDidMount() {
+  //   let url = "http://localhost:3000/events?_page=1";
+  //   fetch(url)
+  //     .then(response => response.json())
+  //     .then(data => {
+  //       let events = data
+  //         .map((event, index) => {
+  //           return (
+  //             <div key={index}>
+  //               <p>Date: {event.date}</p>
+  //               <p>Event Description: {event.description}</p>
+  //             </div>
+  //           );
+  //         })
+  //         .slice(0, 1000);
+  //       this.setState({ events: events });
+  //     });
+  // }
+
   componentDidMount() {
-    let url = "http://localhost:3000/events";
-    fetch(url)
-      .then(response => response.json())
-      .then(data => {
-        let events = data
-          .map((event, index) => {
-            return (
-              <div key={index}>
-                <p>Date: {event.date}</p>
-                <p>Description: {event.description}</p>
-              </div>
-            );
-          })
-          .slice(0, 1000);
-        this.setState({ events: events });
+    axios
+      .get("http://localhost:3000/events?_page=1")
+      .then(response => {
+        console.log("response:", response);
+      })
+      .catch(err => {
+        console.log("err:", err);
       });
   }
 
   render() {
-    return <div className="events"> {this.state.events} </div>;
+    return <div className="events"> </div>;
   }
 }
 export default App;
